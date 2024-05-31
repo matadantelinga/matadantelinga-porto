756f35180d82faddce651e5f6488c7c2e2b0e746
prasojopxl <prasojodesigner@gmail.com>
2024-05-31 12:58:37 +0700

    home v1

---
+++ components/RoomDesign.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RoomDesign() {
+    return (
+        <div className="bg-slate-200 w-full h-[500px] rounded-lg my-24 flex items-center justify-center">RoomDesign</div>
+    )
+}
