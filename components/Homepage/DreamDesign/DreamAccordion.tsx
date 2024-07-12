6fdf57333e91a85322cf5183fd9113434d83d1ba
prasojopxl <prasojodesigner@gmail.com>
2024-07-12 14:01:34 +0700

    add accordion

--- components/Homepage/DreamDesign/DreamAccordion.tsx
+++ components/Homepage/DreamDesign/DreamAccordion.tsx
@@ -1,33 +1,37 @@
 "use client";
-import { Accordion, AccordionContent } from "@/components/ui/accordion";
-import { staticDreamDesignStepper } from "@/lib/staticDataObjects/designStepper";
-import { AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 import React from "react";
 
 export default function DreamStepper() {
   return (
     <>
-      <div className="design-stepper">
-        <ol>
-          {staticDreamDesignStepper.map((item, index) => (
-            <React.Fragment key={index}>
-              <li className="text-c-white">
-                <Accordion type="multiple">
-                  <AccordionItem value="">
-                    <AccordionTrigger>
-                      <div className="title">
-                        {index + 1}. {item.title}
-                      </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      <div className="desc">{item.desc}</div>
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </li>
-            </React.Fragment>
-          ))}
-        </ol>
+      <div className="design-stepper text-white">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="text-blue-500">01 Konsultasi</AccordionTrigger>
+            <AccordionContent className="bg-red-200">
+              Tim akan mengirimkan desain beserta penawaran harga sesuai desain yang sudah OK, dan melakukan pembayaran 50% dari harga penawaran.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>02 Survey Lokasi</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>03 Desain</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     </>
   );
