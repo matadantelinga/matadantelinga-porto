9acf641fe7839f506316ef47ed63e3f0cbda7d6f
prasojopxl <prasojodesigner@gmail.com>
2024-08-13 10:33:48 +0700

    remove console

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({ children, }: { children: React.ReactNode; }
         }
     });
 
-    console.log(pathName)
 
     return (
         <section className="wrapper my-10">
