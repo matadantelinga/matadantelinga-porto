c3f6f5982e02d17da957ea79d6d54b665d4c7417
matadantelinga <indrayanasoetedja@gmail.com>
2024-08-13 17:40:25 +0700

    whats app location function

--- app/location/LocationCard.tsx
+++ app/location/LocationCard.tsx
@@ -6,6 +6,15 @@ interface ICardProps {
 }
 
 export const LocationCard: React.FC<ICardProps> = ({ data }) => {
+  // function to parse number since the value is html format
+  const parseNumber = (value: string): string => {
+    return decodeURIComponent(value).replace(/\s+/g, "");
+  };
+
+  const openWhatsApp = (phoneNumber: string, location: string) => {
+    let url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=Halo%20Planet%20Dekor%2C%20saya%20ingin%20menanyakan%20beberapa%20hal%20terkait%20projek%20desain%20di%20wilayah%20${location}.`;
+    return window.open(url, "_blank");
+  };
   return (
     <>
       <article className="location-card flex align-items-center">
@@ -19,7 +28,12 @@ export const LocationCard: React.FC<ICardProps> = ({ data }) => {
         </div>
         <div className="post-detail">
           <h2 className="title">{data.title}</h2>
-          <div className="flex gap-2 items-center pt-2">
+          <div
+            className="flex gap-2 items-center pt-2 cursor-pointer"
+            onClick={() =>
+              openWhatsApp(parseNumber(data.description), data.title)
+            }
+          >
             <i>
               <img src="/icons/icon-wa.svg" alt="" />
             </i>
